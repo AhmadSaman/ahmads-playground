02e96ea64727bd5077628f8374ce7be288d9d4b9
AhmadSaman <ahmad.00079821@gmail.com>
2022-09-14 23:22:17 +0300

    test automatic batching in react 18.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import Component from "./components/Component";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [count, setCount] = useState(0);
+  console.log("number of rerenders");
+  const handleClick = () => {
+    setCount((prev) => prev + 1);
+    setNumber(count + 1);
+    new Promise((resolve: any) => {
+      setTimeout(() => {
+        setCount(count + 5);
+        setNumber(count + 5);
+        resolve();
+      }, 1000);
+    });
+  };
   return (
     <div className="App">
-      <Component />
+      <p>{count}</p>
+      <p>{number}</p>
+      <button onClick={handleClick}>click</button>
     </div>
   );
 }
